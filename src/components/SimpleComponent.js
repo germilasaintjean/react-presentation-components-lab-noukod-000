0ecacfbd3d4f3c7862922a591ee1cf0d00ced396
Saint-Jean Germila <germila@noukod.com>
2019-04-25 18:45:52 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,10 @@
 // Code SimpleComponent Here
+import React, { Component } from "react";
+
+class SimpleComponent extends Component {
+  // render() {
+  //   return <div className=SimpleComponent> {this.props.mesage} || "i am very happy"</div>;
+  //   }
+  }
+
+export default SimpleComponent
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,13 @@
 // Code SimplerComponent Here
+import React, { Component } from 'react';
+
+class SimplerComponent extends Component {
+
+  render (){
+    rturn (
+
+    )
+  }
+}
+
+export default SimplerComponent
