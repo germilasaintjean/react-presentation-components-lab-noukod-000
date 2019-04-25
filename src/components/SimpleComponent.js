8c2cfea719a919bc9f311f4c30799b67088ff5df
Saint-Jean Germila <germila@noukod.com>
2019-04-25 18:27:24 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,18 @@
 // Code SimpleComponent Here
+import React, { Component } from "react";
+
+class SimpleComponent extends Component {
+     this.state(
+       mood: 'happy'
+     )
+  render () {
+    return (
+      <div>
+      {this.state}
+      </div>
+
+    )
+  }
+}
+
+export default SimpleComponent
